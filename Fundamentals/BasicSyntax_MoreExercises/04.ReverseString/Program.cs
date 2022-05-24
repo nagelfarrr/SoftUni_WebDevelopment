3d2b2c8c8e7500b7f3421c723d2f825a90e48c41
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-05-24 12:20:14 +0300

    Reversed String

---
+++ Fundamentals/BasicSyntax_MoreExercises/04.ReverseString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04.ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            char[] charArray = str.ToCharArray();
+            string reverse = string.Empty;
+
+            for (int i = charArray.Length -1 ; i >= 0; i--)
+            {
+                reverse += charArray[i];
+            }
+            Console.WriteLine(reverse);
+        }
+    }
+}
