408d6aeae3bb70c45fdb5204890b3df41be095b1
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-06-08 17:38:35 +0300

    Softuni

---
+++ Fundamentals/ArraysMoreExercises/02.PascalTriangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02.PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= rows; i++)
+            {
+                int[] arr = new int[i];
+                arr[0] = 1;
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    int[] arr1 = new int[j+i];
+                    Console.WriteLine(string.Join(" ", arr));
+                }
+                
+            }
+        }
+    }
+}
---
+++ Fundamentals/ArraysMoreExercises/EncryptSortAndPrintArray/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace EncryptSortAndPrintArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStrings = int.Parse(Console.ReadLine());
+
+            string vowel = "aoeiuAOEIU";
+
+            bool isVowel = false;
+
+
+
+            int[] sequence = new int[numberOfStrings];
+
+
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                string name = Console.ReadLine();
+                int sum = 0;
+                
+                for (int j = 0; j < name.Length; j++)
+                {
+                    string tempVowel = name[j].ToString();
+                    if (vowel.Contains(tempVowel))
+                    {
+                        isVowel = true;
+                    }
+
+                    if (isVowel)
+                    {
+                        sum += name[j] * name.Length;
+                    }
+                    else
+                    {
+                        sum += name[j] / name.Length;
+                    }
+
+                    isVowel = false;
+                    
+                }
+                sequence[i] = sum;
+            }
+            Array.Sort(sequence);
+            Console.WriteLine(string.Join($"\n", sequence));
+
+        }
+
+    }
+}
