feac551d26d2f3771f2862a0dec67407422ed076
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-06-09 19:05:24 +0300

    SoftUni

---
+++ Fundamentals/Methods/04.PrintingTriangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04.PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int triangleLength = int.Parse(Console.ReadLine());
+            PrintTriangle(triangleLength);
+        }
+
+         static void PrintTriangle(int triangleLength)
+        {
+            for (int i = 1; i <= triangleLength; i++)
+            {
+                PrintLine(1, i);
+            }
+
+            for (int j = triangleLength - 1; j >= 1; j--)
+            {
+                PrintLine(1, j);
+            }
+        }
+
+        static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Fundamentals/Methods/05.Orders/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _05.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int productQuantity = int.Parse(Console.ReadLine());
+
+            PrintPrice(product,productQuantity);
+
+        }
+        static void PrintPrice(string product, int productQuantity)
+        {
+            double productPrice = 0;
+            switch (product)
+            {
+                case "coffee":
+                    productPrice = 1.50;
+                    break;
+                case "water":
+                    productPrice = 1.00;
+                    break;
+                case "coke":
+                    productPrice = 1.40;
+                    break;
+                case"snacks":
+                    productPrice = 2.00;
+                    break;
+            }
+
+            double result = productQuantity * productPrice;
+
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
