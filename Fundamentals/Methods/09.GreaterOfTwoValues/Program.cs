9815e7bcdd3875e646525ddf38a2bbbbe782971d
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-06-09 19:51:44 +0300

    Softuni

---
+++ Fundamentals/Methods/09.GreaterOfTwoValues/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace _09.GreaterOfTwoValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            GetInputAndPrint(type);
+
+        }
+
+        static void GetInputAndPrint(string type)
+        {
+            switch (type)
+            {
+                case "int":
+                    int num1 = int.Parse(Console.ReadLine());
+                    int num2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine(GetMax(num1, num2));
+                    break;
+                case "char":
+                    char char1 = char.Parse(Console.ReadLine());
+                    char char2 = char.Parse(Console.ReadLine());
+                    Console.WriteLine(GetMax(char1,char2));
+                    break;
+                case "string":
+                    string str1 = Console.ReadLine();
+                    string str2 = Console.ReadLine();
+                    Console.WriteLine(GetMax(str1,str2));
+                    break;
+            }
+        }
+
+        static int GetMax(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+        }
+        static char GetMax(char char1, char char2)
+        {
+            if (char1 > char2)
+            {
+                return char1;
+            }
+            else
+            {
+                return char2;
+            }
+        }
+
+        public static string GetMax(string str1, string str2)
+        {
+            int strResult = str1.CompareTo(str2);
+
+            if (strResult < 0)
+            {
+                return str2;
+            }
+            else
+            {
+                return str1;
+            }
+            
+        }
+    }
+}
