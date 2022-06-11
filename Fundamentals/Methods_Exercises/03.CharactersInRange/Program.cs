348f71dd15b393a148bd395271a0a6755e99a5be
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-06-11 13:34:04 +0300

    SoftUni

--- Fundamentals/Methods_Exercises/03.CharactersInRange/Program.cs
+++ Fundamentals/Methods_Exercises/03.CharactersInRange/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _03.CharactersInRange
 {
@@ -6,7 +7,23 @@ namespace _03.CharactersInRange
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char char1 = char.Parse(Console.ReadLine());
+            char char2= char.Parse(Console.ReadLine());
+            PrintMidCharacters(char1, char2);
+        }
+
+        static void PrintMidCharacters(char char1, char char2)
+        {
+            if (char2 < char1)
+            {
+                (char1, char2) = (char2, char1);
+            }
+            
+            for (int i = char1 +1 ; i < char2; i++)
+            {
+                char newchar = (char)i;
+                Console.Write(newchar + " ");
+            }
         }
     }
 }
---
+++ Fundamentals/Methods_Exercises/04.PasswordValidator/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _04.PasswordValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            bool isPasswordLengthValid = ValidPasswordLength(input);
+            bool isPasswordCharactersValid = ValidPasswordCharacters(input);
+            bool isPasswordDigitCountValid = ValidPasswordDigitCount(input);
+
+            if (ValidPasswordLength(input) && ValidPasswordCharacters(input) && ValidPasswordDigitCount(input))
+            {
+                Console.WriteLine("Password is valid");
+            }
+
+            if (!isPasswordLengthValid)
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            }
+
+            if (!isPasswordCharactersValid)
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+            }
+
+            if (!isPasswordDigitCountValid)
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+            }
+            
+        }
+
+        static bool ValidPasswordLength(string input)
+        {
+            return input.Length >= 6 && input.Length <= 10;
+
+        }
+
+        static bool ValidPasswordCharacters(string input)
+        {
+            foreach (char ch in input)
+            {
+                if (!char.IsLetterOrDigit(ch))
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+        static bool ValidPasswordDigitCount(string input)
+        {
+
+            int digitCount = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsDigit(input[i]))
+                {
+                    digitCount++;
+                }
+            }
+
+            return digitCount >= 2;
+        }
+    }
+}
