e574d94e175e6c7e35229e29910a2d3e9182b8d3
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-06-11 12:08:56 +0300

    SoftUni

---
+++ Fundamentals/Methods_Exercises/01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            PrintSmallestNumber(num1,num2,num3);
+        }
+
+        static void PrintSmallestNumber(int num1, int num2, int num3)
+        {
+            int[] arr = { num1, num2, num3 };
+            Array.Sort(arr);
+            Console.WriteLine(arr[0]);
+        }
+    }
+}
---
+++ Fundamentals/Methods_Exercises/02.VowelsCount/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _02.VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(VowelCounts(input));
+        }
+
+        static int VowelCounts(string str)
+        {
+            int count = 0;
+            string vowels = "aoeiu";
+
+            foreach (char ch in str.ToLower())
+            {
+                if (vowels.Contains(ch))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
