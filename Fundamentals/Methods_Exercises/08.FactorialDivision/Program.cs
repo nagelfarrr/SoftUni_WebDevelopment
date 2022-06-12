f36afb09166f8631979215d9b921205e371a57ad
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-06-12 21:51:30 +0300

    SoftUni

--- Fundamentals/Methods_Exercises/08.FactorialDivision/Program.cs
+++ Fundamentals/Methods_Exercises/08.FactorialDivision/Program.cs
@@ -6,7 +6,31 @@ namespace _08.FactorialDivision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            CalculatingFactorials(num1, num2);
+        }
+
+        static void CalculatingFactorials(int num1, int num2)
+        {
+            double firstFactorial = 1;
+
+            for (int i = num1; i > 0; i--)
+            {
+                firstFactorial *= i;
+            }
+
+            double secondFactorial = 1;
+
+            for (int i = num2; i > 0; i--)
+            {
+                secondFactorial *= i;
+            }
+
+            double result = (firstFactorial / secondFactorial);
+            Console.WriteLine($"{result:f2}");
+
         }
     }
 }
---
+++ Fundamentals/Methods_Exercises/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace _09.PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            IsNumberPalindrome(input);
+        }
+
+        static void IsNumberPalindrome(string input)
+        {
+            while (input != "END")
+            {
+                char[] inputArray = input.ToCharArray();
+                bool isEqual = false;
+
+                for (int i = 0; i < inputArray.Length; i++)
+                {
+                    Array.Reverse(inputArray);
+                    if (input[i] == inputArray[i])
+                    {
+                        isEqual = true;
+                    }
+                    else
+                    {
+                        isEqual = false;
+                        break;
+                    }
+
+                }
+
+                if (isEqual)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
