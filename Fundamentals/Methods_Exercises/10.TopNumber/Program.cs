19c3f34615b5b0c3ae261531eb16e0d454456476
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-06-14 18:13:40 +0300

    SoftUni

--- Fundamentals/Methods_Exercises/10.TopNumber/Program.cs
+++ Fundamentals/Methods_Exercises/10.TopNumber/Program.cs
@@ -9,76 +9,38 @@ namespace _10.TopNumber
         static void Main(string[] args)
         {
             int number = int.Parse(Console.ReadLine());
-
-            PrintTopNumber(number);
-
-
-
+            IsSumDivisible(number);
         }
 
-        static void PrintTopNumber(int number)
+
+        static void IsSumDivisible(int number)
         {
-            bool isThereOddDigit = IsThereOddDigit(number);
-            bool isSumOfDigitsDivisble = IsSumOfDigitsDivisble(number);
-
-            for (int i = 1; i <= number; i++)
-            {
-                if (isThereOddDigit && isSumOfDigitsDivisble)
-                {
-                    Console.Write(i);
-                }
-            }
-        }
-        static bool IsThereOddDigit(int number)
-        {
-            bool isThereOddDigit = false;
-            while (number > 0)
-            {
-                int currentDigit = number % 10;
-                if (currentDigit % 2 != 0)
-                {
-                    isThereOddDigit = true;
-                    break;
-
-                }
-                else
-                {
-                    isThereOddDigit = false;
-                }
-
-                number = number / 10;
-            }
-
-
-            //for (int i = 0; i < endValue.Length; i++)
-            //{
-            //    int currentDigit = endValue[i];
-            //
-            //    
-            //}
-
-            if (isThereOddDigit) return true;
-            else return false;
-        }
-
-        static bool IsSumOfDigitsDivisble(int number)
-        {
-            int sumOfDigits = 0;
-
+            int divislbeNumber = 0;
+            int[] numbersArray = new int[0];
             for (int i = 1; i <= number; i++)
             {
                 int tempNumber = i;
-                sumOfDigits = 0;
-                while (tempNumber > 0)
+
+
+
+                if (tempNumber % 8 == 0)
                 {
-                    int currentNumber =tempNumber % 10;
-                    sumOfDigits += currentNumber;
-                    tempNumber /= 10;
+                    divislbeNumber = tempNumber;
+
+                    for (int j = 0; j < numbersArray.Length+1; j++)
+                    {
+                        int[] newArray = new int[j+1];
+                        newArray[j] = divislbeNumber;
+                        numbersArray = newArray;
+                    }
+
                 }
+
+
             }
-            
-            if (sumOfDigits % 8 == 0) return true;
-            else return false;
+
+
         }
+
     }
 }
