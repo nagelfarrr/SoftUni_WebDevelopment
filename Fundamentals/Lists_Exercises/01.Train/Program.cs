6629022b0b0cf69d8bf53f5eae9ca89fec6b83fb
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-06-19 21:28:28 +0300

    SoftUni

---
+++ Fundamentals/Lists_Exercises/01.Train/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> train = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int wagonCapacity = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end") break;
+                int token = 0;
+                string[] command = input.Split();
+                if (command[0] == "Add")
+                {
+                    token = int.Parse(command[1]);
+                    train.Add(token);
+                }
+                else
+                {
+                    AddPassengers(train, wagonCapacity, token, command);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", train));
+        }
+
+        static void AddPassengers(List<int> train, int wagonCapacity, int token, string[] command)
+        {
+            token = int.Parse(command[0]);
+            for (int i = 0; i < train.Count; i++)
+            {
+                if (train[i] + token <= wagonCapacity)
+                {
+                    train[i] = train[i] + token;
+                    break;
+                }
+
+            }
+        }
+
+    }
+}
---
+++ Fundamentals/Lists_Exercises/02.ChangeList/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string input = String.Empty;
+
+            while (input != "end")
+            {
+                input = Console.ReadLine();
+                string[] tokens = input.Split();
+
+                switch (tokens[0])
+                {
+                    case "Delete":
+                        int elementToRemove = int.Parse(tokens[1]);
+                        RemoveElement(numbers, elementToRemove);
+                        break;
+                    case "Insert":
+                        int elementToInsert = int.Parse(tokens[1]);
+                        int listIndex = int.Parse(tokens[2]);
+                        InsertElement(numbers, elementToInsert, listIndex);
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void InsertElement(List<int> numbers, int element, int listIndex)
+        {
+            numbers.Insert(listIndex,element);
+        }
+
+        static void RemoveElement(List<int> numbers, int element)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == element)
+                {
+                    numbers.Remove(numbers[i]);
+                }
+            }
+        }
+    }
+}
