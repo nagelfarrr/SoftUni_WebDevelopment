457640c4637e03d9e1b041e834a26362bc037124
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-06-19 22:30:09 +0300

    SoftUni

---
+++ Fundamentals/Lists_Exercises/03.HouseParty/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.HouseParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
