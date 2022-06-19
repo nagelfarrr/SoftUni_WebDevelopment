d97b68309e2baf638b717d982b80203e44b96e1a
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-06-19 12:42:03 +0300

    SoftUni

---
+++ Fundamentals/Listis/04.ListOfProducts/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Channels;
+
+namespace _04.ListOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfProducts = int.Parse(Console.ReadLine());
+            List<string> productList = new List<string>();
+            for (int i = 0; i < numberOfProducts; i++)
+            {
+                string product = Console.ReadLine();
+                productList.Add(product);
+                
+            }
+
+            productList.Sort();
+            for (int i = 0; i < productList.Count; i++)
+            {
+                Console.WriteLine($"{i+1}.{productList[i]}");
+            }
+        }
+    }
+}
---
+++ Fundamentals/Listis/05.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> newList = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] >0)
+                {
+                    newList.Add(numbers[i]);
+                }
+            }
+
+            newList.Reverse();
+            
+            if (newList.Count <= 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ",newList));
+            }
+        }
+    }
+}
