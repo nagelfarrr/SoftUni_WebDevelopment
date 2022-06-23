c2516eb147a2beaede3f74d9cf2b5fb3fd1b02bc
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-06-23 22:12:07 +0300

    SoftUni

---
+++ Fundamentals/Mid_Exam_Preparation/01.ComputerStore/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _01.ComputerStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            double totalPrice = 0;
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "special" || input == "regular")
+                {
+                    break;
+                }
+                double price = double.Parse(input);
+
+                if (price < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                    continue;
+                }
+                else
+                {
+                    totalPrice += price;
+                }
+                
+            }
+
+            double taxes = totalPrice * 0.2;
+            double finalPrice = totalPrice + taxes;
+            if (finalPrice <= 0)
+            {
+                Console.WriteLine("Invalid order!"); 
+                return;
+            }
+            if (input == "special")
+            {
+                finalPrice *= 0.9;
+            }
+
+            Console.WriteLine($"Congratulations you've just bought a new computer!\n" +
+                              $"Price without taxes: {totalPrice:f2}$\n" +
+                              $"Taxes: {taxes:f2}$\n" +
+                              $"-----------\n" +
+                              $"Total price: {finalPrice:f2}$");
+
+
+        }
+    }
+}
---
+++ Fundamentals/Mid_Exam_Preparation/02.TheLift/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.TheLift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleWaiting = int.Parse(Console.ReadLine());
+            List<int> stateOfLift = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            const int MAXPEOPLE = 4;
+            
+
+            for (int wagon = 0; wagon < stateOfLift.Count; wagon++)
+            {
+                for (int i = stateOfLift[wagon]; i < MAXPEOPLE; i++)
+                {
+                    stateOfLift[wagon]++;
+                    peopleWaiting--;
+
+                    if (peopleWaiting == 0)
+                    {
+                        if (stateOfLift.Sum() < (stateOfLift.Count) * MAXPEOPLE)
+                        {
+                            Console.WriteLine("The lift has empty spots!");
+                        }
+
+                        Console.WriteLine(string.Join(" ",stateOfLift));
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine($"There isn't enough space! {peopleWaiting} people in a queue!");
+            Console.WriteLine(string.Join(" ", stateOfLift));
+            
+        }
+    }
+}
