8df046a104e355d292bb7150941c190035aa8936
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-06-25 00:47:43 +0300

    SoftUni

---
+++ Fundamentals/Mid_Exam_Preparation/04.SoftUniReception/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04.SoftUniReception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int secondEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int thirdEmployeeEfficiency = int.Parse(Console.ReadLine());
+
+            int numberOfStudents = int.Parse(Console.ReadLine());
+
+            int allEmployees = firstEmployeeEfficiency + secondEmployeeEfficiency + thirdEmployeeEfficiency;
+            int timeNeeded = 0;
+            int hoursCount = 0;
+            while (numberOfStudents > 0)
+            {
+                hoursCount++;
+                timeNeeded++;
+                if (hoursCount > 3)
+                {
+                    hoursCount = 0;
+                    
+                    numberOfStudents += allEmployees;
+                }
+
+                numberOfStudents = numberOfStudents - allEmployees;
+            }
+
+
+
+
+            Console.WriteLine($"Time needed: {timeNeeded}h.");
+        }
+    }
+}
---
+++ Fundamentals/Mid_Exam_Preparation/05.Numbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> newList = new List<int>();
+            double average = numbers.Average();
+
+            foreach (var number in numbers)
+            {
+                if (number > average)
+                {
+                    
+                        newList.Add(number);
+                }
+
+            }
+
+            newList.Sort();
+            newList.Reverse();
+            
+            if (newList.Count == 0)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            for (int i = 0; i < newList.Count; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+
+                Console.Write(newList[i] + " ");
+            }
+
+
+        }
+    }
+}
