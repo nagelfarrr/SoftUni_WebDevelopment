741e0fc8707b89b1a68c24d9d39068a90d491605
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-06-25 23:05:08 +0300

    SoftUni

---
+++ Fundamentals/Mid_Exam_Preparation/06.ArrayModifier/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _06.ArrayModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string[] input = Console.ReadLine()
+                .Split();
+            
+            while (true)
+            {
+
+                string command = input[0];
+
+                if (command == "end")
+                {
+                    Console.WriteLine(string.Join(", ", numbers));
+                    break;
+                }
+
+                switch (command)
+                {
+                    case "swap":
+                        int firstIndex = int.Parse(input[1]);
+                        int secondIndex = int.Parse(input[2]);
+                        Swap(numbers, firstIndex, secondIndex);
+                        break;
+                    case "multiply":
+                        int indexOne = int.Parse(input[1]);
+                        int indexTwo = int.Parse(input[2]);
+                        Multiply(numbers, indexOne, indexTwo);
+                        break;
+                    case "decrease":
+                        Decrease(numbers);
+                        break;
+                }
+
+                input = Console.ReadLine().Split();
+
+            }
+        }
+
+        static void Decrease(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                numbers[i]--;
+            }
+        }
+
+        static void Multiply(List<int> numbers, int firstIndex, int secondIndex)
+        {
+            int multipliedNumber = numbers[firstIndex] * numbers[secondIndex];
+            numbers[firstIndex] = multipliedNumber;
+        }
+
+        static void Swap(List<int> numbers, int firstIndex, int secondIndex)
+        {
+            
+            (numbers[firstIndex], numbers[secondIndex]) = (numbers[secondIndex], numbers[firstIndex]);
+
+        }
+    }
+}
