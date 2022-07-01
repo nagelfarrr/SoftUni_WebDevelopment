a2a09244c86c22f7f28ad8811559d9453351b084
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-07-01 20:18:03 +0300

    SoftUni

--- Fundamentals/ObjectsAndClasses/03.Songs/Program.cs
+++ Fundamentals/ObjectsAndClasses/03.Songs/Program.cs
@@ -1,11 +1,15 @@
-﻿namespace _03.Songs
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Songs
 {
     class Program
     {
         static void Main(string[] args)
         {
             int numberOfSongs = int.Parse(Console.ReadLine());
-
+            List<Song> songs = new List<Song>();
             for (int i = 0; i < numberOfSongs; i++)
             {
                 string[] input = Console.ReadLine().Split("_").ToArray();
@@ -14,7 +18,20 @@
                 song.Name = input[1];
                 song.Time = input[2];
 
+                songs.Add(song);
+            }
 
+            string command = Console.ReadLine();
+            foreach (var song in songs)
+            {
+                if (song.TypeList == command)
+                {
+                    Console.WriteLine(song.Name);
+                }
+                else if (command == "all")
+                {
+                    Console.WriteLine(song.Name);
+                }
             }
         }
     }
---
+++ Fundamentals/ObjectsAndClasses/04.Students/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace _04.Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string[] input = Console.ReadLine().Split();
+
+            while (input[0] != "end")
+            {
+                Student student = new Student();
+
+                student.FirstName = input[0];
+                student.LastName = input[1];
+                student.Age = int.Parse(input[2]);
+                student.HomeTown = input[3];
+
+                students.Add(student);
+
+                input = Console.ReadLine().Split();
+            }
+
+            string cityName = Console.ReadLine();
+            foreach (var student in students)
+            {
+                if(student.HomeTown == cityName) Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
+        }
+    }
+
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string HomeTown { get; set; }
+
+    }
+}
