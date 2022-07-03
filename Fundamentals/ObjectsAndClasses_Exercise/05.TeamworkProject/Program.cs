8cffa50f287be72471bd0cdc671ac0d77b4ec059
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-07-03 14:50:07 +0300

    SoftUni

---
+++ Fundamentals/ObjectsAndClasses_Exercise/05.TeamworkProject/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.TeamworkProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int teamCount = int.Parse(Console.ReadLine());
+
+            var teams = new List<Team>();
+
+            for (int i = 0; i < teamCount; i++)
+            {
+                var currTeamInfo = Console.ReadLine().Split('-');
+                var creator = currTeamInfo[0];
+                var teamName = currTeamInfo[1];
+
+                if (teams.Any(team => team.Name == teamName))
+                {
+                    Console.WriteLine($"Team {teamName} was already created!");
+                }
+                else if (teams.Any(team => team.Creator == creator))
+                {
+                    Console.WriteLine($"{creator} cannot create another team!");
+                }
+                else
+                {
+                    var team = new Team();
+                    team.Name = teamName;
+                    team.Creator = creator;
+                    team.Members = new List<string>();
+                    teams.Add(team);
+                    Console.WriteLine($"Team {teamName} has been created by {creator}!");
+                }
+            }
+
+            var line = Console.ReadLine();
+
+            while (line != "end of assignment")
+            {
+                var memberInfo = line.Split(new string[] { "->" }, StringSplitOptions.None);
+                var memberName = memberInfo[0];
+                var teamToJoin = memberInfo[1];
+
+                if (teams.Any(team => team.Members.Contains(memberName)) ||
+                    teams.Any(creator => creator.Creator == memberName))
+                {
+                    Console.WriteLine($"Member {memberName} cannot join team {teamToJoin}!");
+                }
+                else if (teams.All(team => team.Name != teamToJoin))
+                {
+                    Console.WriteLine($"Team {teamToJoin} does not exist!");
+                }
+                else
+                {
+                    var currentTeam = teams.Find(team => team.Name == teamToJoin);
+                    currentTeam.Members.Add(memberName);
+                }
+
+                line = Console.ReadLine();
+            }
+
+            var completedTeams = teams.Where(x => x.Members.Count > 0);
+            var disbandedTeams = teams.Where(team => team.Members.Count == 0);
+
+            foreach (var team in completedTeams.OrderByDescending(x=> x.Members.Count).ThenBy(y=> y.Name))
+            {
+                Console.WriteLine($"{team.Name}");
+                Console.WriteLine($"- {team.Creator}");
+                foreach (var member in team.Members.OrderBy(x=>x))
+                {
+                    Console.WriteLine($"-- {member}");
+                }
+            }
+
+            Console.WriteLine($"Teams to disband:");
+            foreach (var team in disbandedTeams.OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{team.Name}");
+            }
+
+        }
+    }
+
+    class Team
+    {
+        public string Name { get; set; }
+        public string Creator { get; set; }
+        public List<string> Members { get; set; }
+
+    }
+}
---
+++ Fundamentals/ObjectsAndClasses_Exercise/06.VehicleCatalogue/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.VehicleCatalogue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+            int averageTrucksHp = 0;
+            List<Vehicle> trucks = new List<Vehicle>();
+            List<Vehicle> cars = new List<Vehicle>();
+            int averageCarsHp = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "End") break;
+                string[] tokens = input.Split(' ');
+
+                var vehicle = new Vehicle()
+                {
+                    Type = tokens[0],
+                    Model = tokens[1],
+                    Color = tokens[2],
+                    Horsepower = int.Parse(tokens[3])
+                };
+                vehicles.Add(vehicle);
+                if (vehicle.Type == "truck")
+                {
+                    averageTrucksHp += vehicle.Horsepower;
+                }
+                else
+                {
+                    averageCarsHp += vehicle.Horsepower;
+                }
+                
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Close the Catalogue") break;
+
+                foreach (var vehicle in vehicles)
+                {
+                    if (input == vehicle.Model)
+                    {
+                        Console.WriteLine($"Type: {vehicle.Type}\nModel: {vehicle.Model}\nColor: {vehicle.Color}\nHorsepower: {vehicle.Horsepower}");
+                    }
+                }
+            }
+
+
+            averageTrucksHp = averageTrucksHp / trucks.Count;
+            averageCarsHp = averageCarsHp / cars.Count;
+            Console.WriteLine($"Cars have average horsepower of: {averageCarsHp}.");
+            Console.WriteLine($"Trucks have average horsepower of: {averageTrucksHp}.");
+        }
+    }
+
+    public class Vehicle
+    {
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Horsepower { get; set; }
+    }
+}
