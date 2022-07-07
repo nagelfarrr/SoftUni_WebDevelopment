98ab1b9a4ee188de50b1519a19d80c43d65de996
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-07-07 10:48:18 +0300

    SoftUni

---
+++ Fundamentals/AssociativeArrays/02.OddOccurrences/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace _02.OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split()
+                .Select(w => w.ToLower())
+                .ToArray();
+
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (!wordCounts.ContainsKey(word)) wordCounts.Add(word, 0);
+                wordCounts[word] += 1;
+            }
+
+            foreach (var wordCount in wordCounts)
+            {
+                if(wordCount.Value%2 !=0) Console.Write($"{wordCount.Key} ");
+            }
+        }
+    }
+}
---
+++ Fundamentals/AssociativeArrays/03.WordSynonyms/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.WordSynonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfWords = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> synonymDictionary = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < numberOfWords; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                var synonyms = new List<string>();
+                if (!synonymDictionary.ContainsKey(word)) synonymDictionary.Add(word, synonyms);
+                synonymDictionary[word].Add(synonym);
+            }
+
+            foreach (var word in synonymDictionary)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+            }
+
+            
+        }
+    }
+}
