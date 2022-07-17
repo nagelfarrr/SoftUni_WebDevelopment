d20bdd045459872b9d677cef6afae83075f035eb
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-07-17 19:04:26 +0300

    SoftUni

---
+++ Fundamentals/TextProcessing_Exercises/01.ValidUsernames/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ValidUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+            ;
+
+            foreach (var username in usernames)
+            {
+
+                if (username.Length >= 3 && username.Length <= 16)
+                {
+                    bool isValid = true;
+                    for (int i = 0; i < username.Length; i++)
+                    {
+                        char currentChar = username[i];
+
+                        if (!(username.Contains('-') || username.Contains('_') || char.IsLetterOrDigit(currentChar)))
+                        {
+                            isValid = false;
+                            break;
+                        }
+                    }
+                    if (isValid) Console.WriteLine(username);
+                }
+
+            }
+        }
+    }
+}
---
+++ Fundamentals/TextProcessing_Exercises/02.CharacterMultiplier/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _02.CharacterMultiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            string str1 = input[0];
+            string str2 = input[1];
+            int charMultiplier = CharMultiplier(str1, str2);
+            Console.WriteLine(charMultiplier);
+        }
+
+        public static int CharMultiplier(string str1, string str2)
+        {
+            int sum = 0;
+
+            string longest = string.Empty;
+            string shortest = string.Empty;
+
+            if (str1.Length > str2.Length)
+            {
+                longest = str1;
+                shortest = str2;
+            }
+            else
+            {
+                longest = str2;
+                shortest = str1;
+            }
+
+            for (int i = 0; i < shortest.Length; i++)
+            {
+                sum += str1[i] * str2[i];
+            }
+
+            for (int i = shortest.Length; i < longest.Length; i++)
+            {
+                sum += longest[i];
+            }
+
+            return sum;
+        }
+    }
+}
