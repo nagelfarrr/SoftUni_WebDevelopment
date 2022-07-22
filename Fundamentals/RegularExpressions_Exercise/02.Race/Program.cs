7ce240484bb5280e6cd5a8ba681da553c04d994a
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-07-23 00:04:49 +0300

    SoftUni

--- Fundamentals/RegularExpressions_Exercise/02.Race/Program.cs
+++ Fundamentals/RegularExpressions_Exercise/02.Race/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace _02.Race
 {
@@ -6,7 +9,75 @@ namespace _02.Race
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] participants = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string namePatern = @"[A-Za-z]";
+            string digitPatern = @"[0-9]";
+
+            Dictionary<string, int> racerKilometers = new Dictionary<string, int>();
+
+            Regex namePattern = new Regex(namePatern);
+            Regex digitPattern = new Regex(digitPatern);
+
+            string input = Console.ReadLine();
+
+            while (input != "end of race")
+            {
+                MatchCollection nameChars = namePattern.Matches(input);
+                MatchCollection digitChars = digitPattern.Matches(input);
+                string name = String.Empty;
+                string digit = String.Empty;
+                foreach (Match nameChar in nameChars)
+                {
+                    name += nameChar;
+                }
+
+                for (int i = 0; i < digitChars.Count; i++)
+                {
+                    digit += digitChars[i].Value;
+                }
+
+                int digitInInt = int.Parse(digit);
+                int totalKilometers = 0;
+                while (digitInInt > 0)
+                {
+                    int temp = digitInInt % 10;
+                    totalKilometers += temp;
+                    digitInInt = digitInInt / 10;
+                }
+
+                if (participants.Contains(name))
+                {
+                    if (!racerKilometers.ContainsKey(name))
+                    {
+                        racerKilometers[name] = totalKilometers;
+                    }
+                    else
+                    {
+                        racerKilometers[name] += totalKilometers;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            int counter = 0;
+            foreach (var racer in racerKilometers.OrderByDescending(x => x.Value))
+            {
+                counter++;
+                if (counter == 1)
+                {
+                    Console.WriteLine($"{counter}st place: {racer.Key}");
+                }
+                else if (counter == 2)
+                {
+                    Console.WriteLine($"{counter}nd place: {racer.Key}");
+                }
+                else if(counter == 3)
+                {
+                    Console.WriteLine($"{counter}rd place: {racer.Key}");
+                }
+            }
         }
     }
 }
