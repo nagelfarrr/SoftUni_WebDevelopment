ea0a4f56e3f90868ec11ca872038b2c2b787a8ae
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-07-22 18:52:21 +0300

    SoftUni

--- Fundamentals/RegularExpressions_Exercise/01.Furniture/Program.cs
+++ Fundamentals/RegularExpressions_Exercise/01.Furniture/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace _01.Furniture
 {
@@ -6,7 +9,40 @@ namespace _01.Furniture
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string pattern = @">>(?<product>[A-Za-z\s]+)<<(?<price>\d+(.\d+)?)!(?<quantity>\d+)";
+
+            Regex regex = new Regex(pattern);
+
+            List<string> items = new List<string>();
+            double totalPrice = 0.0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Purchase") break;
+
+                Match match = regex.Match(input);
+                if (match.Success)
+                {
+                    string productName = match.Groups["product"].Value;
+                    double price = double.Parse(match.Groups["price"].Value);
+                    int quantity = int.Parse(match.Groups["quantity"].Value);
+                    totalPrice += price * quantity;
+                    items.Add(productName);
+                    
+                }
+            }
+
+            Console.WriteLine("Bought furniture:");
+            if (items.Count > 0)
+            {
+                foreach (var item in items)
+                {
+
+                    Console.WriteLine($"{item}");
+                }
+
+            }
+            Console.WriteLine($"Total money spend: {totalPrice:f2}");
         }
     }
 }
