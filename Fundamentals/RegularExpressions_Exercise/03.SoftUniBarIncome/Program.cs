95c79a95b59a9fcda83a24fce72e09c23178e665
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-07-23 12:05:38 +0300

    SoftUni

---
+++ Fundamentals/RegularExpressions_Exercise/03.SoftUniBarIncome/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03.SoftUniBarIncome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^%(?<customer>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<quantity>[0-9]{1,})\|[^|$%.]*?(?<price>[0-9]+(\.[0-9]+)?)\$";
+
+            Regex regex = new Regex(pattern);
+            double totalIncome = 0.0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end of shift") break;
+                Match match = regex.Match(input);
+                if (match.Success)
+                {
+                    string customer = match.Groups["customer"].Value;
+                    string product = match.Groups["product"].Value;
+                    int quantity = int.Parse(match.Groups["quantity"].Value);
+                    double price = double.Parse(match.Groups["price"].Value);
+
+                    totalIncome += quantity * price;
+                    Console.WriteLine($"{customer}: {product} - {(quantity * price):f2}");
+                }
+            }
+
+            Console.WriteLine($"Total income: {totalIncome:f2}");
+        }
+    }
+}
