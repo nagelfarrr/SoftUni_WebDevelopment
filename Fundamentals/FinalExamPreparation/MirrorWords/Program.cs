49bc651324a3d616afe3c1adf0e4642ea9bd94ce
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-07-31 22:20:36 +0300

    SoftUni

--- Fundamentals/FinalExamPreparation/MirrorWords/Program.cs
+++ Fundamentals/FinalExamPreparation/MirrorWords/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace MirrorWords
 {
@@ -6,7 +9,32 @@ namespace MirrorWords
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            string pattern = @"([@|#])(?<word>[A-Za-z]{4,})\1";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+            List<string> words = new List<string>();
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            foreach (Match match in matches)
+            {
+                words.Add(match.Groups["word"].Value);
+            }
+
+            Console.WriteLine(string.Join(',',words));
+
+            for (int i = 1; i < words.Count; i++)
+            {
+                if (words[i] == words[i-1].Reverse())
+                {
+                    if (!result.ContainsKey(words[i]))
+                        result[words[i-1]] = words[i];
+                }
+            }
+
+            foreach (var word in result)
+            {
+                Console.WriteLine($"{word.Value} <=> {word.Key}");
+            }
         }
     }
 }
