607a460985fb03826b83c8eed9105690051edfbe
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-08-04 11:42:18 +0300

    SoftUni

--- Fundamentals/FinalExamPreparation/FancyBarcode/Program.cs
+++ Fundamentals/FinalExamPreparation/FancyBarcode/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace FancyBarcode
 {
@@ -6,7 +9,53 @@ namespace FancyBarcode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfBarcodes = int.Parse(Console.ReadLine());
+
+            string pattern = @"\@\#{1,}(?<product>[A-Z]{1}[A-Za-z0-9]{4,}[A-Z]{1})\@\#{1,}";
+
+            Regex regex = new Regex(pattern);
+
+            List<string> validItems = new List<string>();
+
+            for (int i = 0; i < numberOfBarcodes; i++)
+            {
+                string input = Console.ReadLine();
+
+                string productGroup = string.Empty;
+
+                Match currMatch = regex.Match(input);
+
+                if (currMatch.Success)
+                {
+                    string currProduct = currMatch.Groups["product"].Value;
+
+                    if (currProduct.Any(char.IsDigit))
+                    {
+                        for (int j = 0; j < currProduct.Length; j++)
+                        {
+                            if (currProduct[j] >= 48 && currProduct[j] <= 57)
+                            {
+                                productGroup += currProduct[j];
+                            }
+                        }
+
+                        Console.WriteLine($"Product group: {productGroup}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Product group: 00");
+                    }
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+
+            }
+
+           
         }
     }
 }
