ec7c8b22e9a291b637dcd630ac32c2e82d372c0d
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-08-20 12:01:39 +0300

    SoftUni

---
+++ Fundamentals/FinalExamPreparation/00.Demo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _00.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double incomePerSearch = double.Parse(Console.ReadLine());
+            int numberOfUsers = int.Parse(Console.ReadLine());
+
+            double totalIncome = 0;
+            for (int i = 1; i <= numberOfUsers; i++)
+            {
+                int numberOfSearches = int.Parse(Console.ReadLine());
+                double totalIncomePerUser = incomePerSearch *numberOfSearches;
+                if (numberOfSearches == 1)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (i % 3 == 0)
+                    {
+                        totalIncomePerUser *=3;
+                    }
+
+                    if (numberOfSearches > 5)
+                    {
+                        totalIncomePerUser *=2;
+                    }
+                    
+                }
+
+                totalIncome += totalIncomePerUser;
+            }
+
+            Console.WriteLine($"Total money earned: {totalIncome:f2}");
+        }
+    }
+}
