eee5ba7bdddc013ee4dba6b1a2f0ac409c5ffc94
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-09-20 12:11:38 +0300

    Stacks and Queues

---
+++ Advanced/02.StacksAndQueues_Exercises/11.KeyRevolver/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.KeyRevolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int barrelSize = int.Parse(Console.ReadLine());
+            int[] bullets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] locks = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int priceValue = int.Parse(Console.ReadLine());
+
+            Queue<int> locksQueue = new Queue<int>(locks);
+            Stack<int> bulletsStack = new Stack<int>(bullets);
+
+            int bulletUsed = 0;
+            while (locksQueue.Count > 0 && bulletsStack.Count > 0)
+            {
+                int reload = barrelSize;
+
+                while (reload > 0)
+                {
+                    if (locksQueue.Count == 0 || bulletsStack.Count == 0) break;
+
+                    int currentBullet = bulletsStack.Pop();
+                    bulletUsed++;
+                    int currentLock = locksQueue.Peek();
+
+                    if (currentBullet <= currentLock)
+                    {
+                        Console.WriteLine("Bang!");
+                        locksQueue.Dequeue();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ping!");
+                    }
+
+                    reload--;
+
+                }
+                if (reload == 0 && bulletsStack.Count > 0)
+                    Console.WriteLine("Reloading!");
+
+            }
+
+            if (bulletsStack.Count >= 0 && locksQueue.Count == 0)
+            {
+                int moneyEarned = priceValue - (bulletUsed * bulletPrice);
+                Console.WriteLine($"{bulletsStack.Count} bullets left. Earned ${moneyEarned}");
+            }
+            else if (locksQueue.Count > 0)
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locksQueue.Count}");
+            }
+        }
+    }
+}
---
+++ Advanced/02.StacksAndQueues_Exercises/12.CupsAndBottles/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12.CupsAndBottles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cups = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] bottles = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int wastedLitters = 0;
+
+
+            Stack<int> bottleStack = new Stack<int>(bottles);
+            Queue<int> cupQueue = new Queue<int>(cups);
+
+            while (true)
+            {
+                if (bottleStack.Count == 0 || cupQueue.Count == 0)
+                    break;
+                int currentBottle = bottleStack.Peek();
+                int currentCup = cupQueue.Peek();
+
+                while (currentCup > 0)
+                {
+                    if (currentBottle >= currentCup)
+                    {
+                        currentBottle--;
+                        currentCup--;
+
+                    }
+                    else
+                    {
+                        currentCup -= currentBottle;
+                        bottleStack.Pop();
+                        currentBottle = bottleStack.Peek();
+                    }
+                }
+
+                if (currentBottle >= currentCup)
+                {
+                    wastedLitters += currentBottle - currentCup;
+                    cupQueue.Dequeue();
+                    bottleStack.Pop();
+                }
+            }
+
+            if (bottleStack.Count > 0)
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", bottleStack)}");
+                Console.WriteLine($"Wasted litters of water: {wastedLitters}");
+            }
+            else if (cupQueue.Count > 0)
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cupQueue)}");
+                Console.WriteLine($"Wasted litters of water: {wastedLitters}");
+            }
+        }
+    }
+}
