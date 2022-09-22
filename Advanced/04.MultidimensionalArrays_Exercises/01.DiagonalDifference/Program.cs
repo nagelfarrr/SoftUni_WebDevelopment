0bb0e76fd2711f2b46eaf2261d033498e715744e
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-09-22 22:58:09 +0300

    Multidimensional Arrays Exercise

---
+++ Advanced/04.MultidimensionalArrays_Exercises/01.DiagonalDifference/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace _01.DiagonalDifference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int squareSize = int.Parse(Console.ReadLine());
+
+            int[,] square = new int[squareSize, squareSize];
+
+            for (int row = 0; row < square.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine().Split().Select(n => int.Parse(n)).ToArray();
+                for (int col = 0; col < square.GetLength(1); col++)
+                {
+                    square[row, col] = rowData[col];
+                }
+            }
+
+            int firstSum = 0;
+            int secondSum = 0;
+
+            for (int row = 0; row < square.GetLength(0); row++)
+            {
+                firstSum += square[row, row];
+
+            }
+
+            int squareRow = 0;
+            for (int col = square.GetLength(1) - 1; col >= 0; col--)
+            {
+                secondSum += square[squareRow, col];
+                squareRow++;
+            }
+
+            Console.WriteLine(Math.Abs(firstSum - secondSum));
+        }
+    }
+}
---
+++ Advanced/04.MultidimensionalArrays_Exercises/02.SquaresInMatrix/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SquaresInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split().Select(n => int.Parse(n)).ToArray();
+
+            char[,] matrix = new char[dimensions[0], dimensions[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] rowData = Console.ReadLine().Split().Select(c => char.Parse(c)).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            int counter = 0;
+            for (int i = 0; i < matrix.GetLength(0) - 1; i++)
+            {
+
+                for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+                {
+                    if (matrix[i, j] == matrix[i, j + 1] && 
+                        matrix[i, j] == matrix[i + 1, j] &&
+                        matrix[i, j] == matrix[i + 1, j + 1])
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
