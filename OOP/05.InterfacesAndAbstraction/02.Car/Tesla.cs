219853743e524852b1b093ad57520f381280bbdd
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2022-11-03 13:19:49 +0200

    Interfaces

---
+++ OOP/05.InterfacesAndAbstraction/02.Car/Tesla.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : IElectricCar
+    {
+        public Tesla(string model, string color, int batteries)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = batteries;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+
+        public void Start()
+        {
+            Console.WriteLine("Engine start");
+
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Breaaak!");
+        }
+
+
+        public override string ToString()
+        {
+            return $"{this.Color} Tesla {this.Model} with {this.Battery} Batteries";
+        }
+    }
+}
