ecfa27d0c1689434c4349e41d52eb86a6ae4c18d
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2023-01-21 11:24:50 +0200

    sql

--- MS_SQL/04.Bult-in-Functions/04.Bult-in-Functions.sql
+++ MS_SQL/04.Bult-in-Functions/04.Bult-in-Functions.sql
@@ -94,3 +94,17 @@ FROM [Employees]
 WHERE [Salary] BETWEEN 10000 AND 50000
 ORDER BY [Salary] DESC
 
+--11. Find All Employees with Rank 2
+
+
+SELECT * FROM
+(
+	SELECT [EmployeeID], [FirstName], [LastName], [Salary],
+		DENSE_RANK() OVER
+		(PARTITION BY [Salary] ORDER BY [EmployeeID])
+		AS [Rank]
+	FROM [Employees]
+) 
+AS [RankedSelection]
+WHERE [Salary] BETWEEN 10000 AND 50000 AND [Rank] = 2
+ORDER BY [Salary] DESC
