dd99b853d3da46f5ab6bbde1b297572081986ff7
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2023-01-25 21:40:12 +0200

    Update .Subqueries_And_Joins.sql

--- MS_SQL/05.Subqueries_And_Joins/.Subqueries_And_Joins.sql
+++ MS_SQL/05.Subqueries_And_Joins/.Subqueries_And_Joins.sql
@@ -25,3 +25,85 @@ JOIN [Addresses] AS [a] ON [e].[AddressID] = [a].[AddressID]
 JOIN [Towns] AS [t] ON [a].[TownID] = [t].[TownID]
 
 ORDER BY [e].[FirstName], [e].[LastName]
+
+--03. Sales Employees
+
+SELECT
+    [e].[EmployeeID]
+    ,[e].[FirstName]
+    ,[e].[LastName]
+    ,[d].[Name] AS [DepartmentName]
+FROM [Employees] AS [e]
+JOIN [Departments] AS [d] ON [e].[DepartmentID] = [d].[DepartmentID]
+    WHERE [d].[Name] = 'Sales'
+ORDER BY [e].[EmployeeID]
+
+--04. Employee Departments
+
+SELECT TOP (5)
+    [e].[EmployeeID]
+    ,[e].[FirstName]
+    ,[e].[Salary]
+    ,[d].[Name] AS [DepartmentName]
+FROM [Employees] AS [e]
+JOIN [Departments] as [d] 
+ON [e].[DepartmentID] = [d].[DepartmentID]
+    WHERE [e].[Salary] > 15000
+ORDER BY [d].[DepartmentID]
+
+--05. Employees Without Projects
+
+SELECT TOP (3)
+    [e].[EmployeeID]
+    ,[e].[FirstName]
+FROM [Employees] AS [e]
+LEFT JOIN [EmployeesProjects] AS [ep]
+ON [e].[EmployeeID] = [ep].[EmployeeID]
+WHERE [ep].[EmployeeID] IS NULL
+ORDER BY [e].[EmployeeID]
+
+--06. Employees Hired After
+
+SELECT
+    [e].[FirstName]
+    ,[e].[LastName]
+    ,[e].[HireDate]
+    ,[d].[Name] AS [DeptName]
+FROM [Employees] AS [e]
+JOIN [Departments] AS [d]
+    ON [e].[DepartmentID] = [d].[DepartmentID] 
+WHERE
+    YEAR([e].[HireDate]) > 1998
+    AND [d].[Name] = 'Sales'
+    OR [d].[Name] = 'Finance'
+ORDER BY [e].[HireDate]
+
+--07. Employees With Project
+
+SELECT TOP (5)
+    [ep].[EmployeeID]
+    ,[e].[FirstName]
+    ,[p].[Name]
+FROM [Employees] AS [e]
+JOIN [EmployeesProjects] AS [ep]
+    ON [e].[EmployeeID] = [ep].[EmployeeID]
+JOIN [Projects] as [p]
+    ON [ep].[ProjectID] = [p].[ProjectID]
+WHERE [p].[StartDate] > '2002-08-13'
+    AND [p].[EndDate] IS NULL
+
+--08. Employee 24
+
+SELECT
+    [e].[EmployeeID]
+    ,[e].[FirstName]
+    ,CASE 
+        WHEN YEAR([p].[StartDate]) >= 2005 THEN NULL
+        ELSE [p].[Name]
+    END AS [ProjectName]
+FROM [Employees] AS [e]
+JOIN [EmployeesProjects] as [ep]
+    ON [e].[EmployeeID] = [ep].[EmployeeID]
+JOIN [Projects] AS [p]
+    ON [ep].[ProjectID] = [p].[ProjectID]
+WHERE [ep].[EmployeeID] = 24
