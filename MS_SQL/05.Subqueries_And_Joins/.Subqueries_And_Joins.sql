94324f75b2b9f9436b6931bdb67b2ff71e5c0472
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2023-01-26 13:14:25 +0200

    Update .Subqueries_And_Joins.sql

--- MS_SQL/05.Subqueries_And_Joins/.Subqueries_And_Joins.sql
+++ MS_SQL/05.Subqueries_And_Joins/.Subqueries_And_Joins.sql
@@ -147,3 +147,43 @@ FROM
     FROM [Employees]
     GROUP BY [DepartmentID]
 ) AS AverageSalary
+
+--------------------------------------
+
+USE [Geography]
+
+--------------------------------------
+
+--12. Highest Peaks in Bulgaria
+
+SELECT
+    [c].[CountryCode]
+    ,[m].[MountainRange]
+    ,[p].[PeakName]
+    ,[p].[Elevation]
+FROM 
+    [Countries] AS [c]
+JOIN [MountainsCountries] AS [mc]
+    ON [c].[CountryCode] = [mc].[CountryCode]
+JOIN [Mountains] AS [m]
+    ON [mc].[MountainId] = [m].[Id]
+JOIN [Peaks] AS [p]
+    ON [m].[Id] = [p].[MountainId]
+WHERE
+    [c].[CountryCode] = 'BG'
+    AND [p].[Elevation] > 2835
+ORDER BY [p].[Elevation] DESC
+
+--13. Count Mountain Ranges
+
+SELECT
+    [mc].[CountryCode]
+    ,COUNT([m].[MountainRange]) AS [MountainRanges]
+FROM
+    [Countries] AS [c]
+JOIN [MountainsCountries] AS [mc]
+    ON [c].[CountryCode] = [mc].[CountryCode]
+JOIN [Mountains] AS [m]
+    ON [mc].[MountainId] = [m].[Id]
+WHERE [mc].[CountryCode] IN ('BG', 'RU', 'US')
+GROUP BY [mc].[CountryCode]
