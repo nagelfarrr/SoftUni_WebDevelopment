e21325fa88e99e3193b4e82c8e95ca2448f13c90
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2023-02-02 22:16:26 +0200

    Update Indices_And_Data_Aggregation.sql

--- MS_SQL/06.Indices_And_Data_Aggregation/Indices_And_Data_Aggregation.sql
+++ MS_SQL/06.Indices_And_Data_Aggregation/Indices_And_Data_Aggregation.sql
@@ -35,3 +35,67 @@ SELECT
     ,SUM(DepositAmount) AS TotalSum
 FROM WizzardDeposits
 GROUP BY DepositGroup
+
+--06. Deposits Sum for Ollivander Family
+
+SELECT
+    DepositGroup
+    ,SUM(DepositAmount) AS TotalSum
+FROM WizzardDeposits
+GROUP BY DepositGroup,MagicWandCreator
+HAVING MagicWandCreator = 'Ollivander family'
+
+--07. Deposits Filter
+
+SELECT
+    DepositGroup
+    ,SUM(DepositAmount) AS TotalSum
+FROM WizzardDeposits
+GROUP BY DepositGroup,MagicWandCreator
+HAVING 
+    MagicWandCreator = 'Ollivander family' 
+    AND SUM(DepositAmount) < 150000
+ORDER BY TotalSum DESC
+
+--08. Deposit Charge
+
+SELECT
+    DepositGroup
+    ,MagicWandCreator
+    ,MIN(DepositCharge) AS NubDeoisutCharge
+FROM WizzardDeposits
+GROUP BY DepositGroup,MagicWandCreator
+ORDER BY MagicWandCreator,DepositGroup
+
+--09. Age Groups
+
+SELECT
+    ag.AgeGroup
+    ,COUNT(*) AS WizardCount
+FROM(
+SELECT
+    CASE
+        WHEN Age <= 10 THEN '[0-10]'
+        WHEN Age <= 20 THEN '[11-20]'
+        WHEN Age <= 30 THEN '[21-30]'
+        WHEN Age <= 40 THEN '[31-40]'
+        WHEN Age <= 50 THEN '[41-50]'
+        WHEN Age <= 60 THEN '[51-60]'
+            ELSE '[61+]' 
+    END AS AgeGroup
+FROM WizzardDeposits
+) as ag
+GROUP BY AgeGroup
+
+--10. First Letter
+SELECT
+    *
+FROM
+(
+    SELECT
+        SUBSTRING( FirstName,1,1) AS FirstLetter
+    FROM WizzardDeposits
+    WHERE DepositGroup = 'Troll Chest'
+) AS fl
+GROUP BY FirstLetter
+ORDER BY FirstLetter
