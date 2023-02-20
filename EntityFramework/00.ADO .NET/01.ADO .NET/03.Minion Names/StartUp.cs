ccf3441b49049949abd03e66691935818933b6ac
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2023-02-21 00:01:49 +0200

    ado .net

--- EntityFramework/00.ADO .NET/01.ADO .NET/03.Minion Names/StartUp.cs
+++ EntityFramework/00.ADO .NET/01.ADO .NET/03.Minion Names/StartUp.cs
@@ -1,1 +1,42 @@
-﻿
+﻿using System.Text;
+using Microsoft.Data.SqlClient;
+
+SqlConnection connection =
+    new SqlConnection(@"Server=DESKTOP-KQ1NJBD\SQLEXPRESS;Database=MinionsDB;Trusted_Connection=True;Integrated Security = true;TrustServerCertificate = true");
+
+connection.Open();
+
+string input = "1";    
+
+using (connection)
+{
+    string queryString = @$"
+                            SELECT
+	                            v.Name,
+	                            m.Name,
+	                            m.Age
+                            FROM Villains AS v
+                            JOIN MinionsVillains AS mv
+	                            ON v.Id = mv.VillainId
+                            JOIN Minions AS m
+	                            ON mv.MinionId = m.Id
+                            WHERE v.Id = {input}
+                            ORDER BY m.Name";
+
+
+    string queryResult = GetQueryResult(queryString, connection);
+}
+
+string GetQueryResult(string queryString, SqlConnection con)
+{
+    SqlCommand command = new SqlCommand(queryString, con);
+
+    using SqlDataReader reader = command.ExecuteReader();
+
+    StringBuilder sb = new StringBuilder();
+
+    while (reader.Read())
+    {
+        
+    }
+}
