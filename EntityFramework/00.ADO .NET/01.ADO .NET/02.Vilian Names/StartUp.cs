5fe406481baa0382ffe2b7ced9df007f8dcb656a
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2023-02-20 10:42:16 +0200

    ADO .NET

---
+++ EntityFramework/00.ADO .NET/01.ADO .NET/02.Vilian Names/StartUp.cs
@@ -0,0 +1,48 @@
+﻿namespace ADO_.NET
+{
+    using System.Text;
+    using Microsoft.Data.SqlClient;
+
+    public class StartUp
+    {
+        static async Task Main(string[] args)
+        {
+            SqlConnection connection =
+                new SqlConnection(@"Server=DESKTOP-KQ1NJBD\SQLEXPRESS;Database=MinionsDB;Trusted_Connection=True;Integrated Security = true;TrustServerCertificate = true");
+
+            connection.Open();
+            await using (connection)
+            {
+                string queryString = @"
+                                SELECT v.Name,
+	                                   COUNT(*) AS NumberOfMinions
+                                  FROM Villains AS v
+                                  JOIN MinionsVillains AS mv ON v.Id = mv.VillainId
+                                  JOIN Minions AS m ON mv.MinionId = m.Id
+                              GROUP BY v.Name
+                                HAVING COUNT(*) > 3
+                              ORDER BY NumberOfMinions DESC";
+
+                string queryResult = GetQueryResult(queryString, connection);
+
+                Console.WriteLine(queryResult);
+            }
+            connection.Close();
+        }
+
+        private static string GetQueryResult(string queryString, SqlConnection connection)
+        {
+            SqlCommand command = new SqlCommand(queryString, connection);
+
+            using SqlDataReader sqlReader = command.ExecuteReader();
+            StringBuilder sb = new StringBuilder();
+
+            while (sqlReader.Read())
+            {
+                sb.AppendLine(sqlReader["Name"] + " - " + sqlReader["NumberOfMinions"]);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
