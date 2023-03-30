294dde4ff9cf076de8f2d5be6e583460dcc87c71
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2023-03-30 13:31:15 +0300

    exam prep

--- EntityFramework/07.ExamPreparation/02.Footballers/Footballers/DataProcessor/Serializer.cs
+++ EntityFramework/07.ExamPreparation/02.Footballers/Footballers/DataProcessor/Serializer.cs
@@ -1,17 +1,81 @@
 ﻿namespace Footballers.DataProcessor
 {
     using Data;
+    using Footballers.DataProcessor.ExportDto;
+    using Newtonsoft.Json;
+    using System.Globalization;
+    using System.Text;
+    using System.Xml.Linq;
+    using System.Xml.Serialization;
 
     public class Serializer
     {
         public static string ExportCoachesWithTheirFootballers(FootballersContext context)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("Coaches");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ExportCoachesDto[]), xmlRoot);
+
+            ExportCoachesDto[] coaches = context.Coaches
+                .Where(c => c.Footballers.Any())
+                .Select(c => new ExportCoachesDto()
+                {
+                    CoachName = c.Name,
+                    FootballersCount = c.Footballers.Count(),
+                    Footballers = c.Footballers.Select(f=> new ExportFootballerDto()
+                    {
+                        Name = f.Name,
+                        Position = f.PositionType.ToString(),
+                    })
+                    .OrderBy(f=> f.Name)
+                    .ToArray()
+
+                })
+                .OrderByDescending(c=> c.FootballersCount)
+                .ThenBy(c=> c.CoachName)
+                .ToArray();
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using (StringWriter writer = new StringWriter(sb))
+            {
+                serializer.Serialize(writer, coaches, namespaces);
+            }
+
+            return sb.ToString().TrimEnd(); ;
         }
 
         public static string ExportTeamsWithMostFootballers(FootballersContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            var teams = context.Teams
+                .Where(t => t.TeamsFootballers.Any(f => f.Footballer.ContractStartDate >= date))
+                .Select(t => new
+                {
+                    Name = t.Name,
+                    Footballers = t.TeamsFootballers
+                    .Where(f=> f.Footballer.ContractStartDate >= date)
+                    .OrderByDescending(f => f.Footballer.ContractEndDate)
+                    .ThenBy(f => f.Footballer.Name)
+                    .Select(f => new
+                    {
+                        FootballerName = f.Footballer.Name,
+                        ContractStartDate = f.Footballer.ContractStartDate.ToString("d", CultureInfo.InvariantCulture),
+                        ContractEndDate = f.Footballer.ContractEndDate.ToString("d", CultureInfo.InvariantCulture),
+                        BestSkillType = f.Footballer.BestSkillType.ToString(),
+                        PositionType = f.Footballer.PositionType.ToString()
+
+                    }).ToArray()
+
+                })
+                .OrderByDescending(t => t.Footballers.Count())
+                .ThenBy(t => t.Name)
+                .Take(5)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(teams, Formatting.Indented);
         }
     }
 }
--- EntityFramework/07.ExamPreparation/02.Footballers/Footballers/StartUp.cs
+++ EntityFramework/07.ExamPreparation/02.Footballers/Footballers/StartUp.cs
@@ -27,7 +27,7 @@ namespace Footballers
 
             ImportEntities(context, projectDir + @"Datasets/", projectDir + @"ImportResults/");
 
-            //ExportEntities(context, projectDir + @"ExportResults/");
+            ExportEntities(context, projectDir + @"ExportResults/");
 
             using (var transaction = context.Database.BeginTransaction())
             {
