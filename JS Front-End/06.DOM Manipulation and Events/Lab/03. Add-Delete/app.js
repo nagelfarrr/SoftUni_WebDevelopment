bbfb07a34e43379f1a843289016c4ce8f4b4af6b
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2023-12-04 18:39:07 +0200

    js

--- JS Front-End/06.DOM Manipulation and Events/Lab/03. Add-Delete/app.js
+++ JS Front-End/06.DOM Manipulation and Events/Lab/03. Add-Delete/app.js
@@ -1,3 +1,23 @@
 function addItem() {
-    //TODO...
+    const ul = document.querySelector("ul#items");
+    const inputField = document.getElementById("newItemText");
+    const textToAdd = inputField.value;
+
+    const li = document.createElement("li");
+    li.textContent = textToAdd;
+
+    ul.appendChild(li);
+    const a  = document.createElement("a");
+    a.href = "#";
+    a.textContent = "[Delete]";
+
+    li.appendChild(a);
+    inputField.value = "";
+
+    a.addEventListener("click", deleteItem);
+
+    function deleteItem() {
+        a.parentElement.remove();
+    }
+
 }
--- JS Front-End/06.DOM Manipulation and Events/Lab/05. Highlight-Active/app.js
+++ JS Front-End/06.DOM Manipulation and Events/Lab/05. Highlight-Active/app.js
@@ -1,3 +1,16 @@
 function focused() {
-    console.log('TODO:...');
+    let inputElements = Array.from(document.querySelectorAll("input"));
+    
+    inputElements.forEach(element => {
+        element.addEventListener("focus", handleFocus);
+        element.addEventListener("blur", handleBlur);
+    });
+
+    function handleFocus(e) {
+        this.parentNode.classList = "focused";
+       
+    }
+    function handleBlur(e) {
+        this.parentNode.classList = "";
+    }
 }
