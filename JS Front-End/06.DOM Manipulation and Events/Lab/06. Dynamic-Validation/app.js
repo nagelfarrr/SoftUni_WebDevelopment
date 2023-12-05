273aa38731ed251f805e0c73d0defa2e414e48a2
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2023-12-05 17:52:09 +0200

    js

--- JS Front-End/06.DOM Manipulation and Events/Lab/06. Dynamic-Validation/app.js
+++ JS Front-End/06.DOM Manipulation and Events/Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,20 @@
 function validate() {
-    console.log('TODO:...');
+    const inputField = document.getElementById("email");
+
+    inputField.addEventListener("change", validateEmail);
+
+
+    function validateEmail() {
+        const emailValue = inputField.value.toLowerCase();
+        const emailRegex = /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+        const isValidEmail = emailRegex.test(emailValue);
+
+        if (!isValidEmail) {
+            inputField.classList.add("error");
+        } else {
+            inputField.classList.remove("error");
+        }
+    }
+
 }
--- JS Front-End/06.DOM Manipulation and Events/Lab/07. Shopping-Cart/solution.js
+++ JS Front-End/06.DOM Manipulation and Events/Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-   //TODO...
+   const textArea = document.getElementsByTagName("textarea")[0];
+   const addButtons = document.getElementsByClassName("add-product");
+   const checkoutButton = document.getElementsByClassName("checkout")[0];
+
+   const products = [];
+   let totalPrice = 0;
+
+   for (const button of Array.from(addButtons)) {
+      const product = button.parentNode.parentNode;
+      const productName = product.querySelector(".product-title").textContent;
+      const productPrice = product.querySelector(".product-line-price").textContent
+
+      button.addEventListener("click", (e) => {
+         if (!products.includes(productName)) {
+            products.push(productName);
+         }
+         totalPrice += Number(productPrice);
+
+         textArea.textContent += `Added ${productName} for ${Number(productPrice).toFixed(2)} to the cart.\n`;
+      })
+   }
+
+   checkoutButton.addEventListener("click", (e) => {
+      if (products.length < 0) {
+         textArea.textContent = "";
+      } else {
+         textArea.textContent += `You bought ${products.join(", ")} for ${totalPrice.toFixed(2)}.`;
+      }
+      checkoutButton.disabled = true;
+
+      for (const addButton of addButtons) {
+         addButton.disabled = true;
+      }
+   })
 }
