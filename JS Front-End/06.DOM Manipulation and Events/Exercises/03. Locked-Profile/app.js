2936830b89c0b23ce44e3e62db93f3c8ebc8d845
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2023-12-08 19:17:42 +0200

    js

--- JS Front-End/06.DOM Manipulation and Events/Exercises/03. Locked-Profile/app.js
+++ JS Front-End/06.DOM Manipulation and Events/Exercises/03. Locked-Profile/app.js
@@ -1,3 +1,38 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const showMoreBtns = Array.from(document.getElementsByTagName("button"));
+
+    showMoreBtns.forEach(btn => {
+        btn.addEventListener("click", showMore);
+    });
+
+
+    function showMore(e) {
+        const profile = this.parentNode;
+        const hiddenContent = profile.querySelector("div");
+        const radioBtns = profile.querySelectorAll(`input[type="radio"]`);
+
+        let lockedRadioBtn;
+        let unlockedRadioBtn;
+        
+        for (let i = 0; i < radioBtns.length; i++) {
+            if (radioBtns[i].value === "lock") {
+                lockedRadioBtn = radioBtns[i];
+            } else if (radioBtns[i].value === "unlock") {
+                unlockedRadioBtn = radioBtns[i];
+            }
+
+        }
+
+        if (unlockedRadioBtn.checked) {
+
+            if (this.textContent === "Show more") {
+                hiddenContent.style.display = "block";
+                this.textContent = "Hide it";
+            } else {
+                hiddenContent.style.display = "none";
+                this.textContent = "Show more";
+            }
+        }
+    }
+
 }
