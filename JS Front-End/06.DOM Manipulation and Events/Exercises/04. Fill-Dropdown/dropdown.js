87ecc1dbe0fd25c7b4f250a9ae232ca9d0dc4f21
nagelfarrr <106115933+nagelfarrr@users.noreply.github.com>
2023-12-09 11:52:22 +0200

    js

--- JS Front-End/06.DOM Manipulation and Events/Exercises/04. Fill-Dropdown/dropdown.js
+++ JS Front-End/06.DOM Manipulation and Events/Exercises/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    const dropDownMenu = document.getElementById("menu");
+    const text = document.getElementById("newItemText");
+    const textValue = document.getElementById("newItemValue");
+
+
+    const newItem = document.createElement("option");
+    newItem.value = textValue.value;
+    newItem.textContent = text.value;
+    dropDownMenu.appendChild(newItem);
+    text.value = "";
+    textValue.value = "";
+
 }
--- JS Front-End/06.DOM Manipulation and Events/Exercises/05. Encode-and-Decode-Messages/app.js
+++ JS Front-End/06.DOM Manipulation and Events/Exercises/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,46 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const btns = Array.from(document.getElementsByTagName("button"));
+    let encodeBtn;
+    let decodeBtn;
+
+    for (let i = 0; i < btns.length; i++) {
+        if(btns[i].textContent.includes("Encode")){
+            btns[i].classList = "encode";
+            encodeBtn = btns[i];
+        } else {
+            btns[i].classList = "decode";
+            decodeBtn = btns[i];
+        }
+    }
+
+    encodeBtn.addEventListener("click", encode);
+    decodeBtn.addEventListener("click", decode);
+
+    function encode(){
+        const textArea = this.parentNode.querySelector("textarea");
+        const messageToEncode = textArea.value;
+        
+        let encodedMessage = "";
+
+        for (let i = 0; i < messageToEncode.length; i++) {
+            encodedMessage += String.fromCharCode(messageToEncode[i].charCodeAt(0) + 1);
+            
+        }
+        textArea.value = "";
+        decodeBtn.parentNode.querySelector("textarea").value = encodedMessage;
+    }
+    
+    function decode(){
+        const textarea = this.parentNode.querySelector("textarea");
+        const messageToDecode = textarea.value;
+       
+        
+        let decodedMessage =""
+        for (let i = 0; i < messageToDecode.length; i++) {
+            decodedMessage +=String.fromCharCode(messageToDecode[i].charCodeAt(0) - 1)
+            
+        }
+        textarea.value = decodedMessage;
+    }
+
 }
